c7938b9103f13f6cd4e25eb44b69e9c91d69e697
Mark Meyer <mark.meyer@gsa.gov>
2025-05-09 09:57:59 -0400

    make a vertex request schema instead passing lists of messages to makes adding configs and testing easier

--- app/providers/vertex_ai/schemas.py
+++ app/providers/vertex_ai/schemas.py
@@ -2,8 +2,18 @@
 # which we can use until it introduces issues
 
 from typing import List, Optional
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 from vertexai.language_models import TextEmbeddingInput
+from vertexai.generative_models import GenerationConfig, Content, SafetySetting
+
+
+class VertexGenerateRequest(BaseModel):
+    # setting this config to allow the direct use of vertex types 
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+    contents: List[Content]
+    generation_config: GenerationConfig | None = None
+    safety_settings: SafetySetting | None = None
+    stream: Optional[bool] = False
 
 class EmbeddingRequest(BaseModel):
     texts: List[str | TextEmbeddingInput]
--- tests/unit/providers/vertex_al/test_vertex_adapter_to_core.py
+++ tests/unit/providers/vertex_al/test_vertex_adapter_to_core.py
@@ -4,7 +4,7 @@ from unittest import mock
 from app.providers.vertex_ai.adapter_to_core import convert_chat_vertex_response, vertex_embed_reposonse_to_core
 
 @mock.patch("app.providers.vertex_ai.adapter_to_core.datetime")
-def test_vertex_chat_reposonse_to_core(mock_datetime, core_chat_reponse,vertex_chat_response):
+def test_vertex_chat_reposonse_to_core(mock_datetime, core_chat_reponse, vertex_chat_response):
     mock_datetime.now.return_value = datetime(2024, 12, 25)
     converted = convert_chat_vertex_response(vertex_chat_response, model="test-model")
     assert core_chat_reponse == converted
